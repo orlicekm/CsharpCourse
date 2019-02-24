91c332bb9dc16b26e6d03d0f36d40f57793e0e69
Michal Orlíček <orlicekm@gmail.com>
2019-02-24 03:38:20 +0100

    .

---
+++ Lectures/Lecture04/Assets/sln/EntityFramework.DAL.Tests/EntityTypesTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using EntityFramework.DAL.Entities;
+using Xunit;
+
+namespace EntityFramework.DAL.Tests
+{
+    public class EntityTypesTest: IClassFixture<SchoolDbContextTestsSetupFixture>
+    {
+        public EntityTypesTest(SchoolDbContextTestsSetupFixture testContext)
+        {
+            this.testContext = testContext;
+        }
+
+        private readonly SchoolDbContextTestsSetupFixture testContext;
+
+        [Fact]
+        public void TestMethod()
+        {
+
+            using (var cn = new SchoolDbContext())
+            {
+                StudentEntity studentEntity = new StudentEntity
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Jane"
+                };
+
+                AddressEntity addressEntity = new AddressEntity
+                {
+                    Id = Guid.NewGuid(),
+                    City = "Monaco",
+                    StudentId = studentEntity.Id
+                };
+
+                cn.Students.Add(studentEntity);
+                cn.SaveChanges();
+            }
+            var number = testContext.SchoolDbContextSUT.Students.Count();
+            Assert.Equal(0, number);
+        }
+    }
+}
